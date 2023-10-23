d158465d083e0e90a52a77f6116787be23a1f03a
Alvin <alvinyong.22@ichat.sp.edu.sg>
2023-10-23 16:49:51 +0800

    exercise 6

--- Lab2.py
+++ Lab2.py
@@ -25,10 +25,25 @@ def calc_min_max(numbers):
     return [minimum, maximum]
 
 
+def calc_median(numbers):
+    sorted_readings = sorted(numbers)
+    num_readings = len(sorted_readings)
+    if num_readings %2 == 1:
+        median = sorted_readings[num_readings//2]
+    else:
+        middle1 = sorted_readings[num_readings // 2 - 1]
+        middle2 = sorted_readings[num_readings // 2]
+        median = (middle1 + middle2) / 2
+    return median
+
+
+
 user_input_list = get_user_input()
 average = calc_average(user_input_list)
 min_max = calc_min_max(user_input_list)
+median = calc_median(user_input_list)
 
 print(f"Average: {average:.2f}")
 print(f"Minimum: {min_max[0]}")
 print(f"Maximum: {min_max[1]}")
+print(f"Median: {median:.2f}")
